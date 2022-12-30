b5efd8527534d75e0c41cbcaf2ade346849cf9d8
sayedhany <sayed591999@gmail.com>
2022-12-30 03:07:11 +0200

    finish hero, stats and business sections

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
 
-      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1 z-[10]">
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}

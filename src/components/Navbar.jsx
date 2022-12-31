c220aa9e27fa337d4526842f5c7d0b32a42a084c
sayedhany <sayed591999@gmail.com>
2022-12-31 23:38:54 +0200

    finish project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,19 +26,19 @@ const Navbar = () => {
         <img
           src={toggle ? close : menu}
           alt="menu"
-          className="w-[28px] h-[28px] object-contain"
+          className="w-[28px] h-[28px] object-contain cursor-pointer"
           onClick={() => setToggle((prevTogg) => !prevTogg)}
         />
         <div
           className={`${
             !toggle ? "hidden" : "flex"
-          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar `}
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar z-[10]`}
         >
-          <ul className="list-none flex  justify-end items-center flex-1 flex-col">
+          <ul className="list-none flex  justify-end items-center flex-1 flex-col z-[6]">
             {navLinks.map((nav, index) => (
               <li
                 key={nav.id}
-                className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                className={`font-poppins font-medium cursor-pointer text-[16px]  ${
                   active === nav.title ? "text-white" : "text-dimWhite"
                 } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
                 onClick={() => setActive(nav.title)}
